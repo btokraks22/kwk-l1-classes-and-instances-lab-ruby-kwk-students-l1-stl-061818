d8223ace3d19c567cb2929b6acb65f0fb8cbb44c
btokraks22 <bree.v.v.13@gmail.com>
2018-06-22 17:41:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,40 +1,8 @@
 class Dog 
   
-  def intialize(size, characteristic, eye_color, coat_color)
-    @size = size
-    @characteristic = characteristic
-    @eye_color = eye_color
-    @coat_color = coat_color
-end
-
-def size
-  @size
-end
-
-def characteristic
-  @characteristic
-end
-
-def eye_color
-  @eye_color
-end
-
-def coat_color
-  @coat_color
-end
+ end
 
 
-fido = Dog.new("Big", "Loving", "Blue eyes", "Black coat")
-
-puts "Fido is a #{size} dog and he is very #{characteristic}. He has #{eye_color} and a #{coat_color}."
-
-snoopy = Dog.new("Small", "Enthusiatic", "Brown eyes", "Brown coat")
-
-puts "Snoopy is a #{size} dog and he is very #{characteristic}. He has #{eye_color} and a #{coat_color}."
-
-lassie = Dog.new("Large", "Protective", "Hazel eyes", "Brown/Red coat")
-
-puts "Lassie is a #{size} dog and she is very #{characteristic}. She has #{eye_color} and a #{coat_color}."
 
 end 
 
